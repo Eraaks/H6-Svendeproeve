c416a1c7b969432e3a8614d9834fe4b195f7a37e
Xcalera <Xcaleragaming@gmail.com>
2023-11-02 03:27:28 +0100

    Added more design + grips

--- App/svendeproeve_klatreapp/lib/flows/user/tips&tricks/widgets/user_tips_tricks_widget.dart
+++ App/svendeproeve_klatreapp/lib/flows/user/tips&tricks/widgets/user_tips_tricks_widget.dart
@@ -17,7 +17,8 @@ class TipsTricksWidgets extends StatefulWidget {
 }
 
 class _TipsTricksWidgetsState extends State<TipsTricksWidgets> {
-  List<ExerciseModel> excercises = _getAllExercises();
+  List<ExerciseModel> exercises = _getAllExercises();
+  List<GripsModel> grips = _getAllGrips();
 
   @override
   Widget build(BuildContext context) {
@@ -25,48 +26,90 @@ class _TipsTricksWidgetsState extends State<TipsTricksWidgets> {
       backgroundColor: mainBackgroundColor,
       appBar: reusableAppBar(),
       drawer: const Sidebar(),
-      body: Center(
-        child: Wrap(
-          runSpacing: 8,
-          children: [
-            const Text(
-              "Empty Tricks page",
-              style: TextStyle(fontSize: 20),
-              textAlign: TextAlign.center,
-            ),
-            ListView.builder(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: excercises.length,
+      body: Column(
+        children: [
+          const Text(
+            "Exercises",
+            style: TextStyle(fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 200,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.all(12),
+              itemCount: exercises.length,
+              separatorBuilder: (context, index) {
+                return const SizedBox(width: 12);
+              },
               itemBuilder: (context, index) {
-                final excercise = excercises[index];
-                return ListTile(
-                    leading: Image.network(
-                      excercise.assetLocation,
-                      fit: BoxFit.cover,
-                      width: 50,
-                      height: 100,
-                    ),
-                    title: Text(excercise.name),
-                    subtitle: Text(excercise.primaryActivation),
-                    onTap: () {});
+                final exercise = exercises[index];
+                return GestureDetector(
+                  onTap: () {
+                    print(exercise.name);
+                  },
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.network(
+                          exercise.assetLocation,
+                          height: 150,
+                          width: 150,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(exercise.name),
+                    ],
+                  ),
+                );
               },
             ),
-          ],
-        ),
+          ),
+          const Text(
+            "Grips",
+            style: TextStyle(fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 200,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.all(12),
+              itemCount: grips.length,
+              separatorBuilder: (context, index) {
+                return const SizedBox(width: 12);
+              },
+              itemBuilder: (context, index) {
+                final grip = grips[index];
+                return GestureDetector(
+                  onTap: () {
+                    print(grip.name);
+                  },
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.network(
+                          grip.img,
+                          height: 150,
+                          width: 150,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(grip.name),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
-  }
-
-  void searchCenter(String query) {
-    final suggestions = _getAllExercises().where((exercise) {
-      final excerciseName = exercise.name.toLowerCase();
-      final input = query.toLowerCase();
-
-      return excerciseName.contains(input);
-    }).toList();
-
-    setState(() => excercises = suggestions);
   }
 }
 
@@ -110,14 +153,46 @@ List<ExerciseModel> _getAllExercises() {
       sets: 0,
       howTo: null,
     ),
+    const ExerciseModel(
+      name: 'Pullups',
+      assetLocation: 'https://i.stack.imgur.com/AY9Xl.png',
+      benefits: '',
+      includedIn: '',
+      overallTarget: '',
+      primaryActivation: 'ur',
+      secondaryActivation: '',
+      reps: 0,
+      sets: 0,
+      howTo: null,
+    ),
   ];
   return allExercises;
 }
 
-// List<GripsModel> _getAllGrips() {
-//   final allClimbingCenters = [
-//     const ClimbingCenter(country: 'Denmark', place: 'Beta Boulders'),
-//     const ClimbingCenter(country: 'Denmark', place: 'Boulders Syd')
-//   ];
-//   return allClimbingCenters;
-// }
+List<GripsModel> _getAllGrips() {
+  final allGrips = [
+    const GripsModel(
+      name: 'Jugs',
+      img:
+          'https://static.wixstatic.com/media/003ebe_868ef5895e9647f78e1e816043d8da40~mv2.jpeg/v1/fill/w_640,h_640,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/003ebe_868ef5895e9647f78e1e816043d8da40~mv2.jpeg',
+      description: 'Nice',
+    ),
+    const GripsModel(
+        name: 'Pinches',
+        img:
+            'https://static.wixstatic.com/media/003ebe_418dcfcb31bb4375b5c4312f1922c0b4~mv2.jpeg/v1/fill/w_640,h_640,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/003ebe_418dcfcb31bb4375b5c4312f1922c0b4~mv2.jpeg',
+        description: 'Alright'),
+    const GripsModel(
+      name: 'Crimp',
+      img:
+          'https://www.99boulders.com/wp-content/uploads/2018/02/crimp-climbing-hold-1200x675.png',
+      description: 'Bitch',
+    ),
+    const GripsModel(
+        name: 'Pinches',
+        img:
+            'https://static.wixstatic.com/media/003ebe_418dcfcb31bb4375b5c4312f1922c0b4~mv2.jpeg/v1/fill/w_640,h_640,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/003ebe_418dcfcb31bb4375b5c4312f1922c0b4~mv2.jpeg',
+        description: 'Alright'),
+  ];
+  return allGrips;
+}
