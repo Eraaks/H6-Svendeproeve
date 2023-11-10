549bd8d35ca685f58d7d69ef326578740a40f13d
Oliver L. Jensen <Oliverlanggaardjensenprivat@gmail.com>
2023-11-10 11:22:29 +0100

    Initial Commit

--- App/svendeproeve_klatreapp/lib/flows/user/personal/widgets/user_personal_widget.dart
+++ App/svendeproeve_klatreapp/lib/flows/user/personal/widgets/user_personal_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:svendeproeve_klatreapp/flows/app_side_bar/app_side_bar.dart';
 import 'package:svendeproeve_klatreapp/flows/app_top_bar/app_top_bar.dart';
+import 'package:svendeproeve_klatreapp/flows/reusable/reusable_graph_widget.dart';
 import 'package:svendeproeve_klatreapp/services/auth.dart';
 
 final AuthService _auth = AuthService();
@@ -21,11 +22,12 @@ class _PersonalWidgetsState extends State<PersonalWidgets> {
       appBar: reusableAppBar(),
       drawer: _Sidebar,
       body: const Center(
-        child: Text(
-          "Empty Personal Page",
-          style: TextStyle(fontSize: 20),
-          textAlign: TextAlign.center,
-        ),
+        child: Reusable_Graph_Widget(),
+        // child: Text(
+        //   "Empty Personal Page",
+        //   style: TextStyle(fontSize: 20),
+        //   textAlign: TextAlign.center,
+        // ),
       ),
     );
   }
